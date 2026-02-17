37cb320bfd380f6425e88d080ea15caca737ef6a
jifereco <jreyescc27@gmail.com>
2026-02-16 20:02:30 -0500

    Fix Bluetooth imports and Kotlin errors 2

--- android/src/main/kotlin/com/example/my_custom_plugin2/MyCustomPlugin2Plugin.kt
+++ android/src/main/kotlin/com/example/my_custom_plugin2/MyCustomPlugin2Plugin.kt
@@ -1,53 +1,72 @@
 package com.example.my_custom_plugin2
 
+import android.app.Activity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** MyCustomPlugin2Plugin */
 class MyCustomPlugin2Plugin :
     FlutterPlugin,
-    MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
-    private lateinit var bleScanner: BleScanner
+    MethodCallHandler,
+    ActivityAware {
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "my_custom_plugin2")
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
+    private var bleScanner: BleScanner? = null
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "my_custom_plugin2")
         channel.setMethodCallHandler(this)
-        bleScanner = BleScanner(channel)
     }
 
-    override fun onMethodCall(
-        call: MethodCall,
-        result: Result
-    ) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
 
-        
-if (call.method == "getPlatformVersion") {
+        when (call.method) {
 
-    result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            "getPlatformVersion" -> {
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
 
-} else if (call.method == "scanDevices") {
+            "scanDevices" -> {
+                if (activity == null) {
+                    result.error("NO_ACTIVITY", "Activity not attached", null)
+                    return
+                }
 
-    bleScanner.scanForDevices(result)
+                if (bleScanner == null) {
+                    bleScanner = BleScanner(activity!!, channel)
+                }
 
-} else {
+                bleScanner?.scanForDevices(result)
+            }
 
-    result.notImplemented()
-}
-
-
-
-
+            else -> result.notImplemented()
+        }
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    // 🔥 ActivityAware implementation
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
     }
 }
