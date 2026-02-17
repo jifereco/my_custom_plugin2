396722e4cf4c0f07d42f05707ca42f4daf2b595b
jifereco <jreyescc27@gmail.com>
2026-02-17 13:19:37 -0500

    OK V1 con getbondedDevice

--- android/src/main/kotlin/com/example/my_custom_plugin2/MyCustomPlugin2Plugin.kt
+++ android/src/main/kotlin/com/example/my_custom_plugin2/MyCustomPlugin2Plugin.kt
@@ -9,6 +9,14 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import io.flutter.plugin.common.EventChannel
+
+
 class MyCustomPlugin2Plugin :
     FlutterPlugin,
     MethodCallHandler,
@@ -18,6 +26,10 @@ class MyCustomPlugin2Plugin :
     private var activity: Activity? = null
     private var bleScanner: BleScanner? = null
     private lateinit var bondManager: BondManager
+    private var bondReceiver: BroadcastReceiver? = null
+    private var bondEventSink: EventChannel.EventSink? = null
+    private lateinit var context: Context
+
 
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
@@ -26,6 +38,36 @@ class MyCustomPlugin2Plugin :
 
        bleScanner = BleScanner(binding.applicationContext, channel)
        bondManager = BondManager()
+       //context = flutterPluginBinding.applicationContext
+       context = binding.applicationContext
+
+
+
+val bondChannel = EventChannel(
+    binding.binaryMessenger,
+    "my_custom_plugin2/bond_stream"
+)
+
+bondChannel.setStreamHandler(object : EventChannel.StreamHandler {
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        bondEventSink = events
+        registerBondReceiver()
+    }
+
+    override fun onCancel(arguments: Any?) {
+        bondReceiver?.let {
+            context.unregisterReceiver(it)
+        }
+        bondEventSink = null
+    }
+})
+
+
+
+
+
+
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
@@ -79,4 +121,39 @@ class MyCustomPlugin2Plugin :
     override fun onDetachedFromActivity() {
         activity = null
     }
+
+
+
+private fun registerBondReceiver() {
+
+    bondReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            if (intent?.action == BluetoothDevice.ACTION_BOND_STATE_CHANGED) {
+
+                val device =
+                    intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+
+                val bondState =
+                    intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1)
+
+                device?.let {
+                    bondEventSink?.success(
+                        mapOf(
+                            "address" to it.address,
+                            "bondState" to bondState
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    val filter = IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
+    context.registerReceiver(bondReceiver, filter)
+}
+
+
+
 }
